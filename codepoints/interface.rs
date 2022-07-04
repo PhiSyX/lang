699f31139c1f3015a9d23a5d49a4f5e35d979cfd
PhiSyX <738010+PhiSyX@users.noreply.github.com>
2022-07-04 22:36:07 +0200

    feat(codepoints): points de code

---
+++ codepoints/interface.rs
@@ -0,0 +1,164 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+// --------- //
+// Interface //
+// --------- //
+
+pub trait CodePointInterface:
+    PartialEq + Eq + Copy + Clone + ::core::fmt::Debug
+{
+    /// Cast en `char`
+    fn as_char(&self) -> char;
+
+    /// Cast en `u8`
+    fn as_byte(&self) -> u8;
+
+    fn is(&self, unit: char) -> bool;
+
+    /// digit
+    ///
+    /// Un point de code compris entre U+0030 DIGIT ZERO (0) et U+0039 DIGIT
+    /// NINE (9) inclus.
+    fn is_digit(&self) -> bool;
+
+    /// Lettre
+    ///
+    /// Une lettre majuscule ou une lettre minuscule.
+    fn is_letter(&self) -> bool;
+
+    /// Lettre minuscule
+    ///
+    /// Une lettre minuscule.
+    fn is_lowercase_letter(&self) -> bool;
+
+    /// Lettre majuscule
+    ///
+    /// Une lettre majuscule.
+    fn is_uppercase_letter(&self) -> bool;
+
+    /// Un point de code ASCII est un point de code situé dans la plage
+    /// U+0000 NULL à U+007F DELETE, inclusivement.
+    fn is_ascii_code_point(&self) -> bool;
+
+    /// ident-start code point
+    ///
+    /// Une lettre, un point de code non ASCII ou U+005F LOW LINE (_).
+    fn is_ident_start(&self) -> bool;
+
+    /// ident-after-start code point
+    ///
+    /// Un caractère alphanumérique, un point de code non ASCII ou U+005F LOW
+    /// LINE (_).
+    fn is_ident_after_start(&self) -> bool;
+
+    /// ident code point
+    ///
+    /// Un point de code ident de début, un chiffre ou U+002D HYPHEN-MINUS
+    /// (-).
+    fn is_ident(&self) -> bool;
+
+    /// newline
+    ///
+    /// Un point de code U+000A LINE FEED ou U+000D CARRIAGE RETURN ou U+000C
+    /// FORM FEED.
+    fn is_newline(&self) -> bool;
+
+    /// non-ASCII code point
+    ///
+    /// Un point de code dont la valeur est égale ou supérieure à U+0080
+    /// <control>
+    fn is_non_ascii(&self) -> bool;
+
+    /// NULL
+    ///
+    /// Un point de code U+0000 NULL.
+    fn is_null(&self) -> bool;
+
+    /// Espace blanc.
+    ///
+    /// Un saut de ligne, une TABULATION DE CARACTÈRES U+0009 ou un ESPACE
+    /// U+0020.
+    fn is_whitespace(&self) -> bool;
+}
+
+// ----- //
+// Macro //
+// ----- //
+
+macro_rules! impl_cdi {
+    ($($type:ty)|*) => {
+    $(
+    impl CodePointInterface for $type {
+        fn as_char(&self) -> char {
+            *self as char
+        }
+
+        fn as_byte(&self) -> u8 {
+            *self as u8
+        }
+
+        fn is(&self, unit: char) -> bool {
+            *self as char == unit
+        }
+
+        fn is_digit(&self) -> bool {
+            self.as_char().is_ascii_digit()
+        }
+
+        fn is_letter(&self) -> bool {
+            self.is_lowercase_letter() || self.is_uppercase_letter()
+        }
+
+         fn is_lowercase_letter(&self) -> bool {
+            self.is_ascii_lowercase()
+        }
+
+        fn is_uppercase_letter(&self) -> bool {
+            self.is_ascii_uppercase()
+        }
+
+        fn is_ascii_code_point(&self) -> bool {
+            matches!(self.as_char(), '\0'..='\u{007F}')
+        }
+
+        fn is_ident_start(&self) -> bool {
+            self.is_letter() || self.is_non_ascii() || self.is('_')
+        }
+
+        fn is_ident_after_start(&self) -> bool {
+            self.is_ascii_alphanumeric() || self.is_non_ascii() || self.is('_')
+        }
+
+        fn is_ident(&self) -> bool {
+            self.is_ident_start() || self.is_ascii_digit() || self.is('-')
+        }
+
+
+        fn is_newline(&self) -> bool {
+            self.is('\n') || self.is('\x0C') || self.is('\r')
+        }
+
+        fn is_non_ascii(&self) -> bool {
+            *self as u32 >= 0x80
+        }
+
+        fn is_null(&self) -> bool {
+            self.is('\0')
+        }
+
+        fn is_whitespace(&self) -> bool {
+            self.as_char().is_whitespace()
+            // ou self.is_ascii_whitespace()
+        }
+    }
+    )?
+    };
+}
+
+// -------------- //
+// Implémentation // -> Interface
+// -------------- //
+
+impl_cdi! { char | u8 }
---
+++ codepoints/lib.rs
@@ -0,0 +1,260 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+mod interface;
+
+pub use self::interface::CodePointInterface;
+
+// ----- //
+// Macro //
+// ----- //
+
+macro_rules! impl_codepoint {
+    (
+    $(
+        $(#[$attr:meta])*
+        | $ch:literal => $variant:ident
+    )*
+    ) => {
+// ----------- //
+// Énumération //
+// ----------- //
+
+/// Les différents points de code
+#[derive(Debug)]
+#[derive(Copy, Clone)]
+#[derive(PartialEq, Eq)]
+#[allow(clippy::upper_case_acronyms, non_camel_case_types)]
+pub enum CodePoint<Unit> {
+    // NOTE(phisyx): il y a trop d'unités, je ne vais pas m'amuser à toutes
+    // les lister :)
+    Unit(Unit),
+
+    /// Espace blanc.
+    ///
+    /// Peut correspondre à l'une de ces unités ASCII:
+    ///   - U+0009 CHARACTER TABULATION (TAB)
+    ///   - U+000A LINE FEED (LF)       - LIRE NOTE ci-bas.
+    ///   - U+000C FORM FEED (FF)       - LIRE NOTE ci-bas.
+    ///   - U+000D CARRIAGE RETURN (CR) - LIRE NOTE ci-bas.
+    ///   - U+0020 SPACE
+    ///
+    /// NOTE(phisyx): ces unités ne sont pas mis dans cette variante par
+    /// défaut, elles sont définies dans [CodePoint::Newline]. Pour définir ces
+    /// unités en tant qu'espace blanc, il faudra appliquer un
+    /// [filtre](stream::InputStream::define_filter_scan()) explicitement
+    /// à l'initialisation du [flux d'entrée](stream::InputStream).
+    Whitespace(Unit),
+
+    /// Saut de ligne.
+    ///
+    /// Peut correspond à l'une de ces unités ASCII:
+    ///   - U+000A LINE FEED (LF)
+    ///   - U+000C FORM FEED (FF)
+    ///   - U+000D CARRIAGE RETURN (CR)
+    Newline(Unit),
+
+    $(
+        $(#[$attr])*
+        $variant,
+    )*
+
+
+    /// EOF.
+    EOF,
+
+    /// Ignore.
+    // NOTE(phisyx): si une unité a pour valeur cette variante, ce sera la
+    // prochaine variante, qui n'est évidemment pas une même variante
+    // CodePoint::Ignore, d'un flux qui sera retournée.
+    Ignore,
+}
+
+impl<U> CodePoint<U>
+where
+    U: CodePointInterface,
+{
+    pub fn maybe_unit(&self) -> Option<char> {
+        Some(match self {
+            | Self::Unit(codepoint)
+            | Self::Whitespace(codepoint)
+            | Self::Newline(codepoint) => codepoint.as_char(),
+
+            | Self::NULL => return None,
+
+            $(
+                #[allow(unreachable_patterns)]
+                | Self::$variant => $ch,
+            )*
+
+            | Self::EOF | Self::Ignore => return None,
+        })
+    }
+}
+
+impl<U: CodePointInterface> From<U> for CodePoint<U> {
+    fn from(unit: U) -> Self {
+        match unit {
+            | unit if unit.is_newline() => Self::Newline(unit),
+            | unit if unit.is_whitespace() => Self::Whitespace(unit),
+         $( | unit if unit.is($ch) => Self::$variant, )*
+            | _ => Self::Unit(unit),
+        }
+    }
+}
+    };
+}
+
+// -------------- //
+// Implémentation //
+// -------------- //
+
+impl<U> CodePoint<U>
+where
+    U: CodePointInterface,
+{
+    pub fn is_ident(&self) -> bool {
+        self.maybe_unit()
+            .map(|unit| unit.is_ident())
+            .unwrap_or_default()
+    }
+
+    pub fn is_ident_start(&self) -> bool {
+        self.maybe_unit()
+            .map(|unit| unit.is_ident_start())
+            .unwrap_or_default()
+    }
+
+    pub fn is_ident_after_start(&self) -> bool {
+        self.maybe_unit()
+            .map(|unit| unit.is_ident_after_start())
+            .unwrap_or_default()
+    }
+
+    pub fn is_newline(&self) -> bool {
+        self.maybe_unit()
+            .map(|unit| unit.is_newline())
+            .unwrap_or_default()
+    }
+
+    pub fn is_whitespace(&self) -> bool {
+        self.maybe_unit()
+            .map(|unit| unit.is_whitespace())
+            .unwrap_or_default()
+    }
+
+    pub fn is(&self, unit: char) -> bool {
+        self.unit().is(unit)
+    }
+
+    pub fn is_valid(&self) -> bool {
+        !matches!(self, Self::NULL | Self::EOF | Self::Ignore)
+    }
+
+    pub fn unit(&self) -> char {
+        self.maybe_unit()
+            .expect("n'est pas un point de code valide pour notre script.")
+    }
+}
+
+impl_codepoint! {
+    /// Hash
+    ///
+    /// Correspond à l'unité ASCII:
+    ///   - U+0023 NUMBER SIGN (#)
+    |   '#'   =>  NUMBER_SIGN
+
+    /// Asterisk
+    ///
+    /// Correspond à l'unité ASCII:
+    ///   - U+002A ASTERISK (*)
+    |   '*'   =>  ASTERISK
+
+    /// Tiret
+    ///
+    /// Correspond à l'unité ASCII:
+    ///   - U+002D HYPHEN-MINUS (-)
+    |   '-'   =>  HYPHEN_MINUS
+
+    /// Slash
+    ///
+    /// Correspond à l'unité ASCII:
+    ///   - U+002F SOLIDUS (/)
+    |   '/'   =>  SOLIDUS
+
+    /// Slash inversé
+    ///
+    /// Correspond à l'unité ASCII:
+    ///   - U+005C REVERSE SOLIDUS (\)
+    |   '\\'  =>  REVERSE_SOLIDUS
+
+    /// Crochet gauche
+    ///
+    /// Correspond à l'unité ASCII:
+    ///   - U+005B LEFT SQUARE BRACKET ([)
+    |   '['   =>  LEFT_SQUARE_BRACKET
+    /// Crochet droit
+    ///
+    /// Correspond à l'unité ASCII:
+    ///   - U+005D RIGHT SQUARE BRACKET (])
+    |   ']'   =>  RIGHT_SQUARE_BRACKET
+
+    /// Parenthèse gauche
+    ///
+    /// Correspond à l'unité ASCII:
+    ///   - U+0028 LEFT PARENTHESIS (()
+    |   '('   =>  LEFT_PARENTHESIS
+    /// Parenthèse droite
+    ///
+    /// Correspond à l'unité ASCII:
+    ///   - U+0029 RIGHT PARENTHESIS ())
+    |   ')'   =>  RIGHT_PARENTHESIS
+
+    /// Accolade gauche
+    ///
+    /// Correspond à l'unité ASCII:
+    ///   - U+007B LEFT CURLY BRACKET ({)
+    |   '{'   =>  LEFT_CURLY_BRACKET
+    /// Accolade droite
+    ///
+    /// Correspond à l'unité ASCII:
+    ///   - U+007B RIGHT CURLY BRACKET ({)
+    |   '}'   =>  RIGHT_CURLY_BRACKET
+
+    /// Point
+    ///
+    /// Correspond à l'unité ASCII:
+    ///   - U+002E FULL STOP (.)
+    |   '.'   => FULL_STOP
+
+    /// Virgule
+    ///
+    /// Correspond à l'unité ASCII:
+    ///   - U+002C COMMA (,)
+    |   ','   =>  COMMA
+
+    /// Deux point
+    ///
+    /// Correspond à l'unité ASCII:
+    ///    - U+003A COLON (:)
+    |   ':'   =>  COLON
+
+    /// Point virgule
+    ///
+    /// Correspond à l'unité ASCII:
+    ///    - U+003B SEMICOLON (;)
+    |   ';'   =>  SEMICOLON
+
+    /// Égal
+    ///
+    /// Correspond à l'unité ASCII:
+    ///    - U+003D EQUALS SIGN (=)
+    |   '='   => EQUALS_SIGN
+
+    /// NULL.
+    ///
+    /// Correspond à l'unité ASCII:
+    ///   U+0000 NULL
+    |   '\0'  =>  NULL
+}
