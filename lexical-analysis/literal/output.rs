2ee8313df58bc679e45bad1a42685ceca42796bc
PhiSyX <738010+PhiSyX@users.noreply.github.com>
2022-07-07 15:31:51 +0200

    feat(lexer): entier

--- lexical-analysis/literal/output.rs
+++ lexical-analysis/literal/output.rs
@@ -2,9 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-pub trait Input {
-    type Output;
+use super::int::Integer;
 
-    /// Fonction d'analyse.
-    fn parse(self) -> Self::Output;
-}
+pub type IntegerOutput = Integer;
